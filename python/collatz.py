5fcf779be03f44328a7d30e9f6ad5351a092cbf8
maxmcd <max.t.mcdonnell@gmail.com>
2018-07-12 12:44:24 -0400

    Add python dir and collatz

---
+++ python/collatz.py
@@ -0,0 +1,11 @@
+
+def collatz(number):
+    if number == 1:
+        return 1
+    if number % 2 == 0:
+        return collatz(int(number/2))
+    return collatz(number*3 + 1)
+
+assert collatz(100) == 1
+assert collatz(10000) == 1
+assert collatz(124234) == 1
